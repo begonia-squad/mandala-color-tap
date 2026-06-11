bd1cbec8261ddbe9454c2f89f72a5d20eed99dc0
begonia-squad <jeff@supji.com>
2026-06-11 10:08:52 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Mandala Color Tap - service worker
    Bump CACHE on every release so updates roll out automatically. */
-const CACHE = 'mandala-v2.0.0';
+const CACHE = 'mandala-v2.1.1';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './music.mp3', './its-jeff-song.mp3', './tut1.png', './tut2.png', './tut3.png', './tut4.png', './tut5.png', './tut6.png', './tut7.png'];
 
 self.addEventListener('install', (e) => {
